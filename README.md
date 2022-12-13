836a0be52ef171e921f4b25bc1e6f425d13e8dec
Seth Vandenbos <seth.vdbos@gmail.com>
2022-12-13 10:40:46 -0600

    tracker

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## Geolocator Maps
 
-Click [here](https://daboss02.github.io/map-location/) to find your current coordinates
+Click [here](https://daboss02.github.io/gps-tracker/) to find your current coordinates
 
 ![JavaScript](https://img.shields.io/badge/javascript-informational?style=for-the-badge&logo=javascript&logoColor=ffffff&color=ffe600)
 ![HTML](https://img.shields.io/badge/html-informational?style=for-the-badge&logo=html5&logoColor=ffffff&color=ff0019)
--- assets/script/index.js
+++ assets/script/index.js
@@ -16,3 +16,52 @@ const overlay = select('.overlay');
 const loading = select('.loading');
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiZGFib3NzMDIiLCJhIjoiY2xiZ3JuZGo1MGhmajNubXUzcmYyM3RvMSJ9.3XF64pMqvw0Mso4OQv3EDA';
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11',
+  center: [0, 0],
+  pitch: 40,
+  zoom: 16
+ });
+  
+ map.dragPan.disable();
+ map.keyboard.disable();
+ map.scrollZoom.disable();
+ map.doubleClickZoom.disable();
+ map.touchZoomRotate.disable();
+  
+ const marker = new mapboxgl.Marker({
+  color: '#3898ff'
+ });
+  
+ function getLocation(position) {
+  const { longitude, latitude } = position.coords;
+  
+  if (longitude && latitude) {
+  map.setCenter([longitude, latitude]);
+  marker.setLngLat([longitude, latitude]).addTo(map);
+  setTimeout(() => { overlay.style.display = 'none' }, 750);
+  }
+ }
+  
+ function errorHandler(event) {
+  loading.style.animationPlayState = 'paused';
+  console.log(event.message);
+ }
+  
+ const options = {
+  enableHighAccuracy: true,
+  maximumAge: 0
+ };
+  
+ /*
+  The watchPosition() method is used to register a handler function that will
+  be called automatically each time the position of the device changes
+ */
+  
+ if (navigator.geolocation) {
+  navigator.geolocation.watchPosition(getLocation, errorHandler, options);
+ } else {
+  console.log('Geolocation is not supported by your browser');
+ }
